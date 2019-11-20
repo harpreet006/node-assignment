8d1f715c4b55c7204d2ad96484ed4f758a23670b
javir005 <javir@ourdesignz.in>
2019-11-20 10:23:42 +0530

    fs module assignment complete

--- fsmodule/assignment/index.js
+++ fsmodule/assignment/index.js
@@ -23,7 +23,7 @@ fs.readFile('test.txt','utf8',function(err,responce){
 if(err) throw (err)
 	if(responce){
 		var getCount = responce.split(/\r?\n/);
-		console.log(getCount.length,"FILE TOTAL COUNT")
+		console.log(getCount.length,"COUNT THE NUMBER OF ROW INTO THE FILE")
 	}
 })
 
@@ -40,7 +40,7 @@ if(err) throw (err)
 				count++
 			}
 		});
-		console.log(count,'COUNT NUMBER OF THE INTO THE readFile')
+		console.log(count,'COUNT NUMBER OF "THE" INTO THE FILE')
 		// console.log(getCount.length,"FILE TOTAL COUNT")
 	}
 })
@@ -58,8 +58,50 @@ if(err) throw (err)
 		var getCount = responce.split(' ').forEach(function(dara){
 			 count++
 		});
-		console.log(count,'COUNT NUMBER OF THE WORD TO THE FILE')
+		console.log(count,'COUNT NUMBER OF THE WORLD`S INTO THE FILE')
 	}
 })
 
 /*TAKE THE FILE AND COUNT THE NUMBER OF WORD INTO THE FILE  */
+
+
+
+/*COPY DATA FORM ONE FILE TO ANOTHER FILE */
+
+var fs= require('fs')
+fs.readFile('test.txt','utf8',function(err,responce){
+if(err) throw (err)
+	if(responce){
+		fs.writeFile('newfile.txt',responce,function(err,responce){
+			if(err) throw (err)
+				if(responce){
+					console.log("Copy data from `test.txt` to `newfile.txt` file  ")
+				}
+		})
+	}
+})
+/*COPY DATA FORM ONE FILE TO ANOTHER FILE */
+
+
+/*REMOVE THE SPECIFY KEYWORD FORM FILE AND CREATE A NEW FILE TO SAVE DATA */
+
+var fs= require('fs')
+fs.readFile('test.txt','utf8',function(err,responce){
+if(err) throw (err)
+	if(responce){
+		let newAry = []
+		responce.split(" ").forEach(function(indexa){
+			if(err) throw (err)
+			if(indexa){
+				if(indexa.trim()=="Lorem"){
+				 newAry +=" **** "
+				}else{
+					newAry += indexa +" "
+				}
+			}
+		})
+		console.log(newAry,"*****")
+	}
+})
+
+/*REMOVE THE SPECIFY KEYWORD FORM FILE AND CREATE A NEW FILE TO SAVE DATA */
